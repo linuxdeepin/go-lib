a8597ba242ebe8514a3de794c2dd598a2447070d
snyh <snyh@snyh.org>
2013-11-19 13:54:52 +0800

    add NotifyChange function to helper DBusObject emit notify signal

--- dbus/pathch.go
+++ dbus/pathch.go
@@ -1,4 +1,5 @@
 package dbus
+import "log"
 
 import "reflect"
 
@@ -13,3 +14,18 @@ var dbusObject DBusObject
 var dbusObjectInterface = reflect.TypeOf((*DBusObject)(nil)).Elem()
 var introspectProxyType = reflect.TypeOf((*IntrospectProxy)(nil)).Elem()
 var propertyType = reflect.TypeOf((*Property)(nil)).Elem()
+
+func NotifyChange(con *Conn, obj DBusObject, propName string) {
+	value := reflect.ValueOf(obj).FieldByName(propName)
+	if value.IsValid() {
+		inputs := make(map[string]Variant)
+		inputs[propName] = MakeVariant(value.Interface())
+		info := obj.GetDBusInfo()
+		e := con.Emit(ObjectPath(info.ObjectPath), "org.freedesktop.DBus.Properties.PropertiesChanged", info.Interface, inputs, make([]string, 0))
+		if e != nil {
+			log.Print(e)
+		}
+	} else {
+		log.Printf(reflect.TypeOf(obj).String(), "hasn't the ", propName, "property")
+	}
+}
---
+++ dbus/property/normal_property.go
@@ -0,0 +1,41 @@
+package property
+
+import "dlib"
+import "reflect"
+import "dlib/dbus"
+import "log"
+
+type NormalProperty struct {
+	value     interface{}
+	valueType reflect.Type
+	notify    func()
+}
+
+func NewNormalProperty(con *dbus.Conn, path, ifc string, propName string, v interface{}) *NormalProperty {
+	prop := NormalProperty(v, reflect.TypeOf(v), nil)
+	prop.notify = func() {
+		inputs := make(map[string]dbus.Variant)
+		inputs[propName] = dbus.MakeVariant(prop.value)
+		e := con.Emit(dbus.ObjectPath(path), "org.freedesktop.DBus.Properties.PropertiesChanged", ifc, inputs, make([]string, 0))
+		if e != nil {
+			log.Print(e)
+		}
+	}
+	return prop
+}
+
+func (p NormalProperty) Set(v interface{}) {
+	if reflect.TypeOf(v) != p.valueType {
+		panic("This property need type of " + p.valueType.String())
+	}
+	p.value = v
+	p.notify()
+}
+
+func (p NormalProperty) Get() interface{} {
+	return p.value
+}
+
+func (p NormalProperty) GetType() reflect.Type {
+	return p.valueType
+}
