ef52561f60a75536537a7ce7d04566837ed40dcc
snyh <snyh@snyh.org>
2013-11-19 09:40:04 +0800

    update gsetting_test

--- gsettings_test.go
+++ gsettings_test.go
@@ -15,22 +15,25 @@ func TestGSettings(t *testing.T) {
 		s.SetBoolean("active-mini-mode", v)
 	}()
 	changed_times := 0
+	ch := make(chan bool, 2)
 	s.Connect("changed::active-mini-mode", func(s *Settings, name string) {
 		changed_times++
+		ch <- true
 	})
 
 	s.SetBoolean("active-mini-mode", true)
+	<-ch
 	if s.GetBoolean("active-mini-mode") != true {
 		t.Error("SetBoolean failed")
 	}
 	s.SetBoolean("active-mini-mode", false)
+	<-ch
 	if s.GetBoolean("active-mini-mode") != false {
 		t.Error("SetBoolean failed")
 	}
 
-	if changed_times != 2 {
-		fmt.Println("changed_times", changed_times)
-		t.Error("ChangedError")
+	if changed_times != cap(ch) {
+		t.Error(fmt.Sprintf("Should only changed %d but changed %d", cap(ch), changed_times))
 	}
 }
 
@@ -46,22 +49,40 @@ func TestGSettingsWithPath(t *testing.T) {
 		s.SetInt("top-edge-action", v)
 	}()
 	changed_times := 0
+	ch := make(chan bool, 2)
 	s.Connect("changed::top-edge-action", func(s *Settings, name string) {
 		changed_times++
+		ch <- true
 	})
 
-	s.SetInt("top-edge-action", v)
-	if s.GetInt("top-edge-action") != v {
+	s.SetInt("top-edge-action", 0)
+	<-ch
+	if s.GetInt("top-edge-action") != 0 {
 		t.Error("SetInt Error")
 	}
 
-	s.SetInt("top-edge-action", v)
-	if s.GetInt("top-edge-action") != v {
+	s.SetInt("top-edge-action", 1)
+	<-ch
+	if s.GetInt("top-edge-action") != 1 {
 		t.Error("SetInt Error")
 	}
 
-	if changed_times != 2 {
-		fmt.Println("changed_times", changed_times)
-		t.Error("ChangedError")
+	if changed_times != cap(ch) {
+		t.Error(fmt.Sprintf("Should only changed %d but changed %d", cap(ch), changed_times))
+	}
+}
+
+func TestInt(t *testing.T) {
+	s := NewSettings("org.gnome.settings-daemon.peripherals.keyboard")
+	old_d := s.GetUint("delay")
+	old_b := s.GetInt("bell-duration")
+	defer func() {
+		s.SetUint("delay", old_d)
+		s.SetUint("delay", old_b)
+	}()
+	s.SetUint("delay", 50)
+	s.SetInt("bell-duration", 50)
+	if s.GetUint("delay") != 50 || s.GetInt("bell-duration") != 50 {
+		t.Fail()
 	}
 }
