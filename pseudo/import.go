c0a3454748fee4b6c41f921ce7196ca28284e829
snyh <snyh@snyh.org>
2013-11-24 19:25:38 +0800

    add dbus/property in pseudo/import.go

--- pseudo/import.go
+++ pseudo/import.go
@@ -1,12 +1,14 @@
 package pseudo
 
 import "dlib/dbus"
+import "dlib/dbus/property"
 import "dlib"
 import "dlib/gobject-2.0"
 import "dlib/gio-2.0"
 import "dlib/glib-2.0"
 
 func nothing() {
+	_ = property.NewGSettingsPropertyFull
 	_ = gio.DBusConnectionFlagsAuthenticationClient
 	_ = glib.CanInline
 	_ = gobject.NilString
