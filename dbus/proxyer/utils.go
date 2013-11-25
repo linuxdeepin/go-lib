9ee45c7d5d8a58e5b2b5d8bffba822c1fd4bff3b
snyh <snyh@snyh.org>
2013-11-25 15:35:47 +0800

    add utils file

---
+++ dbus/proxyer/utils.go
@@ -0,0 +1,80 @@
+package main
+
+import "strings"
+
+func lower(str string) string   { return strings.ToLower(str[:1]) + str[1:] }
+func upper(str string) string   { return strings.ToUpper(str[:1]) + str[1:] }
+func ifc2obj(ifc string) string { return "/" + strings.Replace(ifc, ".", "/", -1) }
+
+func isValidInterface(s string) bool {
+	if len(s) == 0 || len(s) > 255 || s[0] == '.' {
+		return false
+	}
+	elem := strings.Split(s, ".")
+	if len(elem) < 2 {
+		return false
+	}
+	for _, v := range elem {
+		if len(v) == 0 {
+			return false
+		}
+		if v[0] >= '0' && v[0] <= '9' {
+			return false
+		}
+		for _, c := range v {
+			if !isMemberChar(c) {
+				return false
+			}
+		}
+	}
+	return true
+}
+func isValidMember(s string) bool {
+	if len(s) == 0 || len(s) > 255 {
+		return false
+	}
+	i := strings.Index(s, ".")
+	if i != -1 {
+		return false
+	}
+	if s[0] >= '0' && s[0] <= '9' {
+		return false
+	}
+	for _, c := range s {
+		if !isMemberChar(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isMemberChar(c rune) bool {
+	return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') ||
+		(c >= 'a' && c <= 'z') || c == '_'
+}
+
+func getMember(str string) string {
+	fields := strings.Split(str, ".")
+	if len(fields) < 1 {
+		return ""
+	}
+	r := fields[len(fields)-1]
+	if isValidMember(r) {
+		if INFOS.Config.Target == GoLang {
+			r = strings.ToLower(r)
+		}
+		return r
+	} else {
+		return ""
+	}
+}
+func getQMLPkgName(str string) (r string) {
+	fields := strings.Split(str, ".")
+	for i, field := range fields {
+		if i != 0 {
+			r += "."
+		}
+		r += upper(field)
+	}
+	return
+}
