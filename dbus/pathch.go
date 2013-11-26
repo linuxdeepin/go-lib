ff4668c9f175032a5ab8e37ac4b855a14c146bd1
snyh <snyh@snyh.org>
2013-11-26 23:23:49 +0800

    revise dbus.NotifyChanged to remove the Conn parameter

--- dbus/pathch.go
+++ dbus/pathch.go
@@ -16,15 +16,38 @@ var dbusObjectInterface = reflect.TypeOf((*DBusObject)(nil)).Elem()
 var introspectProxyType = reflect.TypeOf((*IntrospectProxy)(nil)).Elem()
 var propertyType = reflect.TypeOf((*Property)(nil)).Elem()
 
-func NotifyChange(con *Conn, obj DBusObject, propName string) {
+func isExitsInBus(con *Conn, obj DBusObject) bool {
+	con.handlersLck.Lock()
+	defer con.handlersLck.Unlock()
+	info := obj.GetDBusInfo()
+	if ifcs , ok := con.handlers[ObjectPath(info.ObjectPath)]; ok {
+		return ifcs[info.Interface] == obj
+	}
+	return false
+}
+
+func NotifyChange(obj DBusObject, propName string) {
 	value := getValueOf(obj).FieldByName(propName)
 	if value.IsValid() {
-		inputs := make(map[string]Variant)
-		inputs[propName] = MakeVariant(value.Interface())
-		info := obj.GetDBusInfo()
-		e := con.Emit(ObjectPath(info.ObjectPath), "org.freedesktop.DBus.Properties.PropertiesChanged", info.Interface, inputs, make([]string, 0))
-		if e != nil {
-			log.Print(e)
+		systemBusLck.Lock()
+		defer systemBusLck.Unlock()
+		sessionBusLck.Lock()
+		defer sessionBusLck.Unlock()
+
+		var err error
+		if systemBus != nil && isExitsInBus(systemBus, obj) {
+			inputs := make(map[string]Variant)
+			inputs[propName] = MakeVariant(value.Interface())
+			info := obj.GetDBusInfo()
+			err = systemBus.Emit(ObjectPath(info.ObjectPath), "org.freedesktop.DBus.Properties.PropertiesChanged", info.Interface, inputs, make([]string, 0))
+		} else if sessionBus != nil && isExitsInBus(sessionBus, obj) {
+			inputs := make(map[string]Variant)
+			inputs[propName] = MakeVariant(value.Interface())
+			info := obj.GetDBusInfo()
+			err = sessionBus.Emit(ObjectPath(info.ObjectPath), "org.freedesktop.DBus.Properties.PropertiesChanged", info.Interface, inputs, make([]string, 0))
+		}
+		if err != nil {
+			log.Print(err)
 		}
 	} else {
 		log.Printf(reflect.TypeOf(obj).String(), "hasn't the ", propName, "property")
