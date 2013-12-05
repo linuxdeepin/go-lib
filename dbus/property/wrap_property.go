c0897f2c263404cae8c70d5fdee243b5a5d01407
snyh <snyh@snyh.org>
2013-12-05 15:19:51 +0800

    dbus.Property revise WrapProperty with an BaseObserver

--- dbus/property/wrap_property.go
+++ dbus/property/wrap_property.go
@@ -3,19 +3,35 @@ package property
 import "reflect"
 import "dlib/dbus"
 
+type BaseObserver struct {
+	cbs []func()
+}
+
+func (o BaseObserver) ConnectChanged(cb func()) {
+	o.cbs = append(o.cbs, cb)
+}
+
+func (o BaseObserver) Notify() {
+	for _, c := range o.cbs {
+		c()
+	}
+}
+
 type WrapProperty struct {
+	BaseObserver
 	obj  dbus.DBusObject
 	name string
 	core dbus.Property
 }
 
-func NewWrapProperty(obj dbus.DBusObject, propName string, core dbus.Property) WrapProperty {
-	return WrapProperty{obj, propName, core}
+func NewWrapProperty(obj dbus.DBusObject, propName string, core dbus.Property) *WrapProperty {
+	return &WrapProperty{BaseObserver{}, obj, propName, core}
 }
 
 func (p WrapProperty) Set(v interface{}) {
 	p.core.Set(v)
 	dbus.NotifyChange(p.obj, p.name)
+	p.Notify()
 }
 
 func (p WrapProperty) Get() interface{} {
