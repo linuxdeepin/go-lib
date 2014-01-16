2cef9165c68f1e433704d836a2c848fb4f0df139
snyh <snyh@snyh.org>
2014-01-16 15:35:43 +0800

    logger add 'assert' function

--- logger/logger.go
+++ logger/logger.go
@@ -11,3 +11,18 @@ func Println(v ...interface{}) {
 	_, file, line, _ := runtime.Caller(1)
 	log.Printf("%s:%d :%s", file, line, r)
 }
+
+func Printf(format string, v ...interface{}) {
+	r := fmt.Sprintf(format, v...)
+	_, file, line, _ := runtime.Caller(1)
+	log.Printf("%s:%d :%s", file, line, r)
+}
+
+func Assert(exp bool, v ...interface{}) {
+	if exp == false {
+		panic(fmt.Sprintln(v...))
+	}
+}
+func AssertNotReached() {
+	panic("Shouldn't reached")
+}
