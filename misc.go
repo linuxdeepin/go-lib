5902846c1e5ad9da484ba9041f303e77206711d3
snyh <snyh@snyh.org>
2014-04-01 23:51:23 +0800

    dlib: app Unique function; rename loop.go to misc.go

---
+++ misc.go
@@ -0,0 +1,52 @@
+package dlib
+
+import "dlib/dbus"
+
+/*
+#include <glib.h>
+void _run() {
+	g_main_loop_run(g_main_loop_new(0, FALSE));
+}
+#cgo pkg-config: glib-2.0
+*/
+import "C"
+import "fmt"
+
+func StartLoop() {
+	C._run()
+}
+
+const (
+	SystemBus  = 1
+	SessionBus = 2
+)
+
+func UniqueOnSession(name string) bool {
+	con, err := dbus.SessionBus()
+	if err != nil {
+		return false
+	}
+	return uniqueOnAny(con, name)
+}
+func UniqueOnSystem(name string) bool {
+	con, err := dbus.SystemBus()
+	if err != nil {
+		return false
+	}
+	return uniqueOnAny(con, name)
+}
+
+func uniqueOnAny(bus *dbus.Conn, name string) bool {
+	var used bool
+	err := bus.BusObject().Call("NameHasOwner", 0, name).Store(&used)
+	if !used {
+		var i uint32
+		err = bus.BusObject().Call("RequestName", 0, name, uint32(0)).Store(&i)
+		fmt.Println("HUHU", err)
+		if err != nil {
+			return false
+		}
+	}
+	fmt.Println("SD", err)
+	return used == false
+}
