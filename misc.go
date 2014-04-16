96a5d27be10489b20b2eee9774ad561344853439
snyh <snyh@snyh.org>
2014-04-16 23:34:20 +0800

    misc: add gettext function

--- misc.go
+++ misc.go
@@ -5,6 +5,8 @@ import "dlib/dbus"
 /*
 #include <glib.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <libintl.h>
 void _run() {
 	g_main_loop_run(g_main_loop_new(0, FALSE));
 }
@@ -12,6 +14,7 @@ void _init_i18n() { setlocale(LC_ALL, ""); }
 #cgo pkg-config: glib-2.0
 */
 import "C"
+import "unsafe"
 
 func StartLoop() {
 	C._run()
@@ -21,6 +24,17 @@ const (
 	SystemBus  = 1
 	SessionBus = 2
 )
+
+func Tr(id string) string {
+	_id := C.CString(id)
+	defer C.free(unsafe.Pointer(_id))
+	return C.GoString(C.gettext(_id))
+}
+func Textdomain(domain string) {
+	_domain := C.CString(domain)
+	defer C.free(unsafe.Pointer(_domain))
+	C.textdomain(_domain)
+}
 
 func InitI18n() {
 	C._init_i18n()
