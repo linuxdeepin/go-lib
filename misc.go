e9ea8aaf3d756c0823c70e0ca2f30f61bcd9d317
snyh <snyh@snyh.org>
2014-04-21 10:13:35 +0800

    misc: add dgettext function

--- misc.go
+++ misc.go
@@ -30,6 +30,15 @@ func Tr(id string) string {
 	defer C.free(unsafe.Pointer(_id))
 	return C.GoString(C.gettext(_id))
 }
+
+func DGettext(domain, id string) string {
+	_id := C.CString(id)
+	_d := C.CString(domain)
+	defer C.free(unsafe.Pointer(_id))
+	defer C.free(unsafe.Pointer(_d))
+	return C.GoString(C.dgettext(_d, _id))
+}
+
 func Textdomain(domain string) {
 	_domain := C.CString(domain)
 	defer C.free(unsafe.Pointer(_domain))
