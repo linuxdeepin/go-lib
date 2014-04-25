08f9a78460bdecccebb774c12a712acab2f62d34
snyh <snyh@snyh.org>
2014-04-25 12:38:47 +0800

    modified:   import.go

--- pseudo/import.go
+++ pseudo/import.go
@@ -1,11 +1,23 @@
 package pseudo
 
-import _ "dlib/dbus"
-import _ "dlib/dbus/property"
-import _ "dlib"
-import _ "dlib/gobject-2.0"
-import _ "dlib/gio-2.0"
-import _ "dlib/glib-2.0"
-import _ "dlib/logger"
-import _ "dlib/graphic"
-import _ "dlib/utils"
+import "dlib/dbus"
+import "dlib/dbus/property"
+import "dlib"
+import "dlib/gobject-2.0"
+import "dlib/gio-2.0"
+import "dlib/glib-2.0"
+import "dlib/logger"
+import "dlib/graphic"
+import "dlib/utils"
+
+func nothing() {
+	_ = graphic.GetDominantColorOfImage
+	_ = logger.NewLogger
+	_ = property.NewGSettingsBoolProperty
+	_ = gio.DBusConnectionFlagsAuthenticationClient
+	_ = glib.CanInline
+	_ = gobject.NilString
+	_ = dlib.StartLoop
+	_ = dbus.NewConn
+	_ = utils.NewUtils
+}
