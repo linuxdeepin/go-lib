9f66b9a25d3b992382d87bef0b70fb2f69a911e0
Xu FaSheng <fasheng.xu@gmail.com>
2014-06-18 09:27:17 +0800

    utils: update test code

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -26,14 +26,11 @@ func (*UtilsTest) TestIsEnvExists(c *C) {
 func (*UtilsTest) TestUnsetEnv(c *C) {
 	testEnvName := "test_unset_env"
 	testEnvValue := "test_env_value"
-
-	c.Check("", Equals, os.Getenv(testEnvName))
-	os.Setenv(testEnvName, "")
-	c.Check(os.Getenv(testEnvName), Equals, testEnvValue)
-
+	c.Check(false, Equals, IsEnvExists(testEnvName))
+	os.Setenv(testEnvName, testEnvValue)
+	c.Check(true, Equals, IsEnvExists(testEnvName))
 	envCount := len(os.Environ())
 	UnsetEnv(testEnvName)
-	c.Check(os.Getenv(testEnvName), Equals, "")
-	c.Check(len(os.Environ()), Equals, envCount-1)
 	c.Check(false, Equals, IsEnvExists(testEnvName))
+	c.Check(len(os.Environ()), Equals, envCount-1)
 }
