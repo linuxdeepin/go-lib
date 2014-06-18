bbad0199df1867f8534048fd58a2642bde278bb4
Xu FaSheng <fasheng.xu@gmail.com>
2014-06-18 14:49:56 +0800

    utils: remove UnsetEnvC(), ClearEnvC()

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -34,20 +34,3 @@ func (*UtilsTest) TestUnsetEnv(c *C) {
 	c.Check(false, Equals, IsEnvExists(testEnvName))
 	c.Check(len(os.Environ()), Equals, envCount-1)
 }
-
-func (*UtilsTest) TestUnsetEnvC(c *C) {
-	testEnvName := "test_unset_envc"
-	testEnvValue := "test_env_value"
-	c.Check(false, Equals, IsEnvExists(testEnvName))
-	os.Setenv(testEnvName, testEnvValue)
-	c.Check(true, Equals, IsEnvExists(testEnvName))
-	envCount := len(os.Environ())
-	UnsetEnvC(testEnvName)
-	c.Check(false, Equals, IsEnvExists(testEnvName))
-	c.Check(len(os.Environ()), Equals, envCount-1)
-}
-
-func (*UtilsTest) TestClearEnvC(c *C) {
-	ClearEnvC()
-	c.Check(len(os.Environ()), Equals, 0)
-}
