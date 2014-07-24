8754f43ef193858c2ae1a8bf6c19c936e3657573
Xu FaSheng <fasheng.xu@gmail.com>
2014-07-24 17:46:30 +0800

    graphic: refactor code, add save_load.go

--- graphic/save_load.go
+++ graphic/save_load.go
@@ -22,49 +22,42 @@
 package graphic
 
 import (
-	"crypto/md5"
-	"fmt"
 	"image"
-	"image/draw"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"os"
-	"path"
 )
 
-func GenerateCacheFilePath(keyword string) (dstfile string) {
-	graphicCacheFormat := os.Getenv("HOME") + "/.cache/dde/graphic/%s"
-	dstfile = fmt.Sprintf(graphicCacheFormat, encodeMD5Str(keyword))
-	ensureDirExists(path.Dir(dstfile))
+// LoadImage load image file and return image.Image object.
+func LoadImage(imgfile string) (img image.Image, err error) {
+	f, err := os.Open(imgfile)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	img, _, err = image.Decode(f)
 	return
 }
 
-func openFileOrCreate(file string) (*os.File, error) {
-	return os.OpenFile(file, os.O_WRONLY|os.O_CREATE, 0644)
-}
-
-func isFileExists(file string) bool {
-	if _, err := os.Stat(file); err == nil {
-		return true
-	} else {
-		return false
+// SaveImage save image.Image object to target file.
+func SaveImage(dstfile string, m image.Image, f Format) (err error) {
+	df, err := openFileOrCreate(dstfile)
+	if err != nil {
+		return
 	}
+	defer df.Close()
+	return doSaveImage(df, m, f)
 }
 
-func ensureDirExists(dir string) {
-	os.MkdirAll(dir, 0755)
-}
-
-func encodeMD5Str(s string) string {
-	h := md5.New()
-	io.WriteString(h, s)
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-// convert image.Image to *image.RGBA
-func convertToRGBA(img image.Image) (rgba *image.RGBA) {
-	b := img.Bounds()
-	r := image.Rect(0, 0, b.Dx(), b.Dy())
-	rgba = image.NewRGBA(r)
-	draw.Draw(rgba, rgba.Bounds(), img, b.Min, draw.Src)
+func doSaveImage(w io.Writer, m image.Image, f Format) (err error) {
+	switch f {
+	case FormatPng:
+		err = png.Encode(w, m)
+	case FormatJpeg:
+		err = jpeg.Encode(w, m, nil)
+	default:
+		err = png.Encode(w, m)
+	}
 	return
 }
