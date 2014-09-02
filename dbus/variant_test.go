e1a685975a6445e1ad54882e0a256e1e20297c50
snyh <snyh@snyh.org>
2014-09-02 09:18:28 +0800

    dbus/test: fix map testing broken
    The map behavior was changed in golang 1.3, we can't rely on the
    order of map elements even in smaller size maps.
    
    Change-Id: I336aed41cf49a77e83efb5351de3b6388f002953

--- dbus/variant_test.go
+++ dbus/variant_test.go
@@ -16,14 +16,48 @@ var variantFormatTests = []struct {
 	{[]int64{1, 2}, `@ax [1, 2]`},
 	{[][]int32{{3, 4}, {5, 6}}, `[[3, 4], [5, 6]]`},
 	{[]Variant{MakeVariant(int32(1)), MakeVariant(1.0)}, `[<1>, <@d 1>]`},
-	{map[string]int32{"one": 1, "two": 2}, `{"one": 1, "two": 2}`},
-	{map[int32]ObjectPath{1: "/org/foo"}, `@a{io} {1: "/org/foo"}`},
-	{map[string]Variant{}, `@a{sv} {}`},
+}
+var variantFormatMapTests = []struct {
+	v interface{}
+	s []string
+}{
+	{
+		map[string]Variant{},
+		[]string{
+			`@a{sv} {}`,
+		},
+	},
+	{
+		map[int32]ObjectPath{1: "/org/foo"},
+		[]string{
+			`@a{io} {1: "/org/foo"}`,
+		},
+	},
+	{
+		map[string]int32{"one": 1, "two": 2},
+		[]string{
+			`{"one": 1, "two": 2}`,
+			`{"two": 2, "one": 1}`,
+		},
+	},
 }
 
 func TestFormatVariant(t *testing.T) {
 	for i, v := range variantFormatTests {
 		if s := MakeVariant(v.v).String(); s != v.s {
+			t.Errorf("test %d: got %q, wanted %q", i+1, s, v.s)
+		}
+	}
+	for i, v := range variantFormatMapTests {
+		s := MakeVariant(v.v).String()
+		ok := false
+		for _, ss := range v.s {
+			if ss == s {
+				ok = true
+				continue
+			}
+		}
+		if !ok {
 			t.Errorf("test %d: got %q, wanted %q", i+1, s, v.s)
 		}
 	}
