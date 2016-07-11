42fa0786059aec6c753db5433568a42d7be9a62c
electricface <songwentai@deepin.com>
2016-07-11 13:45:11 +0800

    sound: Fix canberra_play_system_sound and canberra_play_sound_file return
    Change-Id: Ifb68f3545bd9e33e750ebbc5d36edafac50704af

--- sound/player.c
+++ sound/player.c
@@ -24,7 +24,6 @@ int
 canberra_play_system_sound(char *theme, char *event_id,
         char *device, char* driver)
 {
-	int ret;
 	int curid = ++id;
 	setlocale(LC_ALL, "");
 	ca_context *ca = connect_canberra_context(device, driver);
@@ -32,28 +31,33 @@ canberra_play_system_sound(char *theme, char *event_id,
 		return -1;
 	}
 
-	ret = ca_context_play(ca, curid,
+	int playret = ca_context_play(ca, curid,
 	                      CA_PROP_CANBERRA_XDG_THEME_NAME, theme,
 	                      CA_PROP_EVENT_ID, event_id, NULL);
+
+	if (playret != CA_SUCCESS) {
+		g_warning("play: id=%d %s\n", curid, ca_strerror(playret));
+		ca_context_destroy(ca);
+		return playret;
+	}
 
 	// wait for end
 	int playing;
 	do {
 		g_usleep(500 * 1000); // sleep 0.5s
-		ret = ca_context_playing(ca, curid, &playing);
+		int ret = ca_context_playing(ca, curid, &playing);
+		if (ret != CA_SUCCESS) {
+			g_warning("ca_context_playing id=%d %s\n", curid, ca_strerror(ret));
+		}
 	} while (playing > 0);
 
 	ca_context_destroy(ca);
-	if (ret != CA_SUCCESS) {
-		g_warning("play: id=%d %s\n", curid, ca_strerror(ret));
-	}
-	return ret;
+	return playret;
 }
 
 int
 canberra_play_sound_file(char *file, char *device, char* driver)
 {
-	int ret;
 	int curid = ++id;
 	setlocale(LC_ALL, "");
 	ca_context *ca = connect_canberra_context(device, driver);
@@ -61,21 +65,27 @@ canberra_play_sound_file(char *file, char *device, char* driver)
 		return -1;
 	}
 
-	ret = ca_context_play(ca, curid,
+	int playret = ca_context_play(ca, curid,
 	                      CA_PROP_MEDIA_FILENAME, file, NULL);
+
+	if (playret != CA_SUCCESS) {
+		g_warning("play_sound_file filename:%s id=%d %s\n", file, curid, ca_strerror(playret));
+		ca_context_destroy(ca);
+		return playret;
+	}
 
 	// wait for end
 	int playing;
 	do {
 		g_usleep(500 * 1000); // sleep 0.5s
-		ret = ca_context_playing(ca, curid, &playing);
+		int ret = ca_context_playing(ca, curid, &playing);
+		if (ret != CA_SUCCESS) {
+			g_warning("ca_context_playing id=%d %s\n", curid, ca_strerror(ret));
+		}
 	} while (playing > 0);
 
 	ca_context_destroy(ca);
-	if (ret != CA_SUCCESS) {
-		g_warning("play filename: id=%d %s\n", curid, ca_strerror(ret));
-	}
-	return ret;
+	return playret;
 }
 
 static ca_context*
