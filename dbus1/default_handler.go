0c6601b9ec9ae37f3bd2aac9da99d64e6de272be
electricface <songwentai@deepin.com>
2018-05-30 10:07:59 +0800

    fix(dbus1): exportedObj data race
    Change-Id: I79b585bab8dd8017a02fc16c082ba3553ae019eb

--- dbus1/default_handler.go
+++ dbus1/default_handler.go
@@ -161,6 +161,7 @@ func newExportedObject() *exportedObj {
 }
 
 type exportedObj struct {
+	mu         sync.Mutex
 	interfaces map[string]*exportedIntf
 }
 
@@ -168,19 +169,28 @@ func (obj *exportedObj) LookupInterface(name string) (Interface, bool) {
 	if name == "" {
 		return obj, true
 	}
+	obj.mu.Lock()
 	intf, exists := obj.interfaces[name]
+	obj.mu.Unlock()
 	return intf, exists
 }
 
 func (obj *exportedObj) AddInterface(name string, iface *exportedIntf) {
+	obj.mu.Lock()
 	obj.interfaces[name] = iface
+	obj.mu.Unlock()
 }
 
 func (obj *exportedObj) DeleteInterface(name string) {
+	obj.mu.Lock()
 	delete(obj.interfaces, name)
+	obj.mu.Unlock()
 }
 
 func (obj *exportedObj) LookupMethod(name string) (Method, bool) {
+	obj.mu.Lock()
+	defer obj.mu.Unlock()
+
 	for _, intf := range obj.interfaces {
 		method, exists := intf.LookupMethod(name)
 		if exists {
