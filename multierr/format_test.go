2be7b5893d4310e976f5f3951057634651e957c1
electricface <songwentai@deepin.com>
2020-01-15 10:34:37 +0800

    change(api): add lib multierr

---
+++ multierr/format_test.go
@@ -0,0 +1,36 @@
+package multierr
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestListFormatFuncSingle(t *testing.T) {
+	expected := `1 error occurred:
+	* foo`
+
+	errs := []error{
+		errors.New("foo"),
+	}
+
+	actual := ListFormatFunc(errs)
+	if actual != expected {
+		t.Fatalf("bad: %#v", actual)
+	}
+}
+
+func TestListFormatFuncMultiple(t *testing.T) {
+	expected := `2 errors occurred:
+	* foo
+	* bar`
+
+	errs := []error{
+		errors.New("foo"),
+		errors.New("bar"),
+	}
+
+	actual := ListFormatFunc(errs)
+	if actual != expected {
+		t.Fatalf("bad: %#v", actual)
+	}
+}
---
+++ multierr/multierr.go
@@ -0,0 +1,73 @@
+package multierr
+
+import (
+	"fmt"
+)
+
+// code copy from github.com/hashicorp/go-multierror
+
+// Error is an error type to track multiple errors. This is used to
+// accumulate errors in cases and return them as a single "error".
+type Error struct {
+	Errors      []error
+	ErrorFormat ErrorFormatFunc
+}
+
+func (e *Error) Error() string {
+	fn := e.ErrorFormat
+	if fn == nil {
+		fn = ListFormatFunc
+	}
+
+	return fn(e.Errors)
+}
+
+// ErrorOrNil returns an error interface if this Error represents
+// a list of errors, or returns nil if the list of errors is empty. This
+// function is useful at the end of accumulation to make sure that the value
+// returned represents the existence of errors.
+func (e *Error) ErrorOrNil() error {
+	if e == nil {
+		return nil
+	}
+	if len(e.Errors) == 0 {
+		return nil
+	}
+
+	return e
+}
+
+func (e *Error) GoString() string {
+	return fmt.Sprintf("*%#v", *e)
+}
+
+// WrappedErrors returns the list of errors that this Error is wrapping.
+// It is an implementation of the errwrap.Wrapper interface so that
+// multierror.Error can be used with that library.
+//
+// This method is not safe to be called concurrently and is no different
+// than accessing the Errors field directly. It is implemented only to
+// satisfy the errwrap.Wrapper interface.
+func (e *Error) WrappedErrors() []error {
+	return e.Errors
+}
+
+func New() *Error {
+	return &Error{}
+}
+
+func (e *Error) Append(errs ...error) {
+	// Go through each error and flatten
+	for _, item := range errs {
+		switch item := item.(type) {
+		case *Error:
+			if item != nil {
+				e.Errors = append(e.Errors, item.Errors...)
+			}
+		default:
+			if item != nil {
+				e.Errors = append(e.Errors, item)
+			}
+		}
+	}
+}
