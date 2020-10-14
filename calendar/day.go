07056b69a4cf13ac61c48adf6d55d2c314e5c52e
lichangze <lichangze@uniontech.com>
2020-10-14 16:55:15 +0800

    fix(calendar): 在纪念日起始年份前不显示该纪念日
    通过在纪念日增加起始年份属性的方式，在获取Festival的时候对纪念日起始日期进行筛选
    
    Log: 在纪念日起始年份前不显示该纪念日
    Bug: https://pms.uniontech.com/zentao/bug-view-31849.html
    Change-Id: Ide1ae336090d9c13db1ee59a65cee039de59726f
    Reviewed-on: http://gerrit.uniontech.com/c/go-lib/+/7498
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: songwentai <songwentai@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- calendar/day.go
+++ calendar/day.go
@@ -19,32 +19,142 @@
 
 package calendar
 
+import "strings"
 
 type Day struct {
 	Year, Month, Day int
 }
 
-var solarFestival = map[int]string{
-	101:  "元旦",
-	214:  "情人节",
-	305:  "学雷锋纪念日",
-	308:  "妇女节",
-	312:  "植树节",
-	401:  "愚人节",
-	415:  "全民国家安全教育日",
-	501:  "劳动节",
-	504:  "青年节",
-	601:  "儿童节",
-	701:  "建党节,香港回归纪念日",
-	801:  "建军节",
-	903:  "抗日战争胜利纪念日",
-	910:  "教师节",
-	1001: "国庆节",
-	1213: "南京大屠杀死难者国家公祭日",
-	1220: "澳门回归纪念",
-	1224: "平安夜",
-	1225: "圣诞节",
-	1226: "毛泽东诞辰纪念",
+type festival struct {
+	name      string
+	startYear int
+}
+
+var solarFestivals = map[int][]festival{
+	101: {
+		{
+			name:      "元旦",
+			startYear: 0,
+		},
+	},
+	214: {
+		{
+			name:      "情人节",
+			startYear: 0,
+		},
+	},
+	305: {
+		{
+			name:      "学雷锋纪念日",
+			startYear: 1963,
+		},
+	},
+	308: {
+		{
+			name:      "妇女节",
+			startYear: 0,
+		},
+	},
+	312: {
+		{
+			name:      "植树节",
+			startYear: 0,
+		},
+	},
+	401: {
+		{
+			name:      "愚人节",
+			startYear: 0,
+		},
+	},
+	415: {
+		{
+			name:      "全民国家安全教育日",
+			startYear: 0,
+		},
+	},
+	501: {
+		{
+			name:      "劳动节",
+			startYear: 0,
+		},
+	},
+	504: {
+		{
+			name:      "青年节",
+			startYear: 0,
+		},
+	},
+	601: {
+		{
+			name:      "儿童节",
+			startYear: 0,
+		},
+	},
+	701: {
+		{
+			name:      "建党节",
+			startYear: 0,
+		},
+		{
+			name:      "香港回归纪念日",
+			startYear: 1997,
+		},
+	},
+	801: {
+		{
+			name:      "建军节",
+			startYear: 0,
+		},
+	},
+	903: {
+		{
+			name:      "抗日战争胜利纪念日",
+			startYear: 1945,
+		},
+	},
+	910: {
+		{
+			name:      "教师节",
+			startYear: 0,
+		},
+	},
+	1001: {
+		{
+			name:      "国庆节",
+			startYear: 0,
+		},
+	},
+	1213: {
+		{
+			name:      "南京大屠杀死难者国家公祭日",
+			startYear: 1937,
+		},
+	},
+	1220: {
+		{
+			name:      "澳门回归纪念",
+			startYear: 1999,
+		},
+	},
+	1224: {
+		{
+			name:      "平安夜",
+			startYear: 0,
+		},
+	},
+	1225: {
+		{
+			name:      "圣诞节",
+			startYear: 0,
+		},
+	},
+	1226: {
+		{
+			name:      "毛泽东诞辰纪念",
+			startYear: 0,
+		},
+	},
 }
 
 func (d *Day) Festival() string {
@@ -52,42 +162,47 @@ func (d *Day) Festival() string {
 	month := d.Month
 	day := d.Day
 	name := ""
-	if (month == 5) || (month == 6){
+	var festivals []string
+	if (month == 5) || (month == 6) {
 		name = festivalForFatherAndMother(year, month, day)
-		if name != ""{
-			return name
+		if name != "" {
+			festivals = append(festivals, name)
 		}
 	}
-	key := month * 100 + day
-	if name, ok := solarFestival[key]; ok {
-		return name
+	key := month*100 + day
+	if solarFestival, ok := solarFestivals[key]; ok {
+		for _, festival := range solarFestival {
+			if festival.startYear <= year {
+				festivals = append(festivals, festival.name)
+			}
+		}
 	}
-	return ""
+	return strings.Join(festivals, ",")
 }
 
-func  festivalForFatherAndMother(year, month, day int) string {
-	var disparityMotherDay,disparityFatherDay,fatherDay,i,motherDay int
+func festivalForFatherAndMother(year, month, day int) string {
+	var disparityMotherDay, disparityFatherDay, fatherDay, i, motherDay int
 	var leapYear int
-	for i = 1900 ; i <= year ;i++{
-		if( ( i % 400 == 0 ) || ( ( i % 100 != 0 ) && ( i % 4 == 0 ) ) ){
+	for i = 1900; i <= year; i++ {
+		if (i%400 == 0) || ((i%100 != 0) && (i%4 == 0)) {
 			leapYear = leapYear + 1
 		}
 	}
 	if month == 5 {
-		disparityMotherDay = ( ( ( year - 1899 ) * 365 + leapYear ) -( 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 ) ) % 7
+		disparityMotherDay = (((year-1899)*365 + leapYear) - (31 + 30 + 31 + 31 + 30 + 31 + 30 + 31)) % 7
 		motherDay = 14 - disparityMotherDay
 		if day == motherDay {
 			return "母亲节"
-		}else{
+		} else {
 			return ""
 		}
 	}
 	if month == 6 {
-		disparityFatherDay = ( ( ( year - 1899 ) * 365 + leapYear ) -(  30 + 31 + 31 + 30 + 31 + 30 + 31 ) ) % 7
-		fatherDay =  21 - disparityFatherDay
+		disparityFatherDay = (((year-1899)*365 + leapYear) - (30 + 31 + 31 + 30 + 31 + 30 + 31)) % 7
+		fatherDay = 21 - disparityFatherDay
 		if day == fatherDay {
 			return "父亲节"
-		}else{
+		} else {
 			return ""
 		}
 	}
