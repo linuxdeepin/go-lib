f07ac04d3e1961d242eab7472785dbc681f14254
luokai <luokai@uniontech.com>
2021-06-22 16:07:16 +0800

    test: 添加单元测试
    添加单元测试
    
    Log: 添加单元测试
    Change-Id: I81388f6ab5e360da21a338d4b0f4019267be3356

---
+++ cgroup/cgroup_test.go
@@ -0,0 +1,78 @@
+package cgroup
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var mountTableTest = []*MountTableItem{
+	{
+		name:        "name=systemd",
+		mountPoints: []string{"/sys/fs/cgroup/systemd"},
+	},
+	{
+		name:        "hugetlb",
+		mountPoints: []string{"/sys/fs/cgroup/hugetlb"},
+	},
+	{
+		name:        "freezer",
+		mountPoints: []string{"/sys/fs/cgroup/freezer"},
+	},
+	{
+		name:        "memory",
+		mountPoints: []string{"/sys/fs/cgroup/memory"},
+	},
+	{
+		name:        "net_cls",
+		mountPoints: []string{"/sys/fs/cgroup/net_cls,net_prio"},
+	},
+	{
+		name:        "net_prio",
+		mountPoints: []string{"/sys/fs/cgroup/net_cls,net_prio"},
+	},
+	{
+		name:        "cpu",
+		mountPoints: []string{"/sys/fs/cgroup/cpu,cpuacct"},
+	},
+	{
+		name:        "cpuacct",
+		mountPoints: []string{"/sys/fs/cgroup/cpu,cpuacct"},
+	},
+	{
+		name:        "cpuset",
+		mountPoints: []string{"/sys/fs/cgroup/cpuset"},
+	},
+	{
+		name:        "rdma",
+		mountPoints: []string{"/sys/fs/cgroup/rdma"},
+	},
+	{
+		name:        "blkio",
+		mountPoints: []string{"/sys/fs/cgroup/blkio"},
+	},
+	{
+		name:        "devices",
+		mountPoints: []string{"/sys/fs/cgroup/devices"},
+	}, {
+		name:        "perf_event",
+		mountPoints: []string{"/sys/fs/cgroup/perf_event"},
+	},
+	{
+		name:        "pids",
+		mountPoints: []string{"/sys/fs/cgroup/pids"},
+	},
+}
+
+func Test_MountTableItem(t *testing.T) {
+	err := Init()
+	assert.Nil(t, err)
+	for _, mountTableItem := range mountTable {
+		for _, mountTest := range mountTableTest {
+			if mountTableItem.Name() == mountTest.name {
+				assert.Equal(t, mountTableItem.MountPoints(), mountTest.mountPoints)
+				assert.Equal(t, GetSubSysMountPoint(mountTableItem.name), mountTest.mountPoints[0])
+				assert.Equal(t, GetSubSysMountPoints(mountTableItem.name), mountTest.mountPoints)
+			}
+		}
+	}
+}
