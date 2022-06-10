0f3588e2dceb5a502a2642377678e8c198346fd2
tanfeng <tanfeng@uniontech.com>
2022-06-10 22:01:53 +0800

    fix: 修复关闭音效总开关，音效列表没有被隐藏的问题
    一个dbus实例导出到多个dbusinterface时，gsetting的dbusinterface的回调只有一个，所以被最后一个dbusinterface覆盖，改成列表，可以给每个dbusinterface触发回调
    
    Log: 修复关闭音效总开关，音效列表没有被隐藏的问题
    Influence: 控制中心音效总开关
    Bug: https://pms.uniontech.com/bug-view-139203.html
    Change-Id: If0f0741b8ad4af9f381ec4fa0bd5833605af3185

--- dbusutil/gsprop/gsprop.go
+++ dbusutil/gsprop/gsprop.go
@@ -6,16 +6,16 @@ import (
 	"sync"
 
 	"github.com/godbus/dbus"
+	gio "github.com/linuxdeepin/go-gir/gio-2.0"
 	"github.com/linuxdeepin/go-lib/dbusutil"
 	"github.com/linuxdeepin/go-lib/gsettings"
-	"github.com/linuxdeepin/go-gir/gio-2.0"
 )
 
 type base struct {
-	mu            sync.Mutex
-	gs            *gio.Settings
-	key           string
-	notifyChanged func(val interface{})
+	mu                sync.Mutex
+	gs                *gio.Settings
+	key               string
+	notifyChangedList []func(val interface{})
 }
 
 func (b *base) bind(gs *gio.Settings, keyName string,
@@ -28,19 +28,22 @@ func (b *base) bind(gs *gio.Settings, keyName string,
 	gs.GetProperty("path", &propPath)
 	gsettings.ConnectChanged(propPath, keyName, func(_ string) {
 		b.mu.Lock()
-		notifyChanged := b.notifyChanged
+		notifyChangedList := b.notifyChangedList
 		b.mu.Unlock()
-
-		if notifyChanged != nil {
+		if notifyChangedList != nil {
 			val, _ := getFn()
-			notifyChanged(val)
+			for _, notifyChanged := range notifyChangedList {
+				if notifyChanged != nil {
+					notifyChanged(val)
+				}
+			}
 		}
 	})
 }
 
 func (b *base) SetNotifyChangedFunc(fn func(val interface{})) {
 	b.mu.Lock()
-	b.notifyChanged = fn
+	b.notifyChangedList = append(b.notifyChangedList, fn)
 	b.mu.Unlock()
 }
 
