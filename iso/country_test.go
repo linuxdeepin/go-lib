29d203ded4029a9c36f3321182adcf94af603927
chenhongtao <chenhongtao@uniontech.com>
2023-03-20 18:27:15 +0800

    chore: test update

--- iso/country_test.go
+++ iso/country_test.go
@@ -5,10 +5,9 @@
 package iso
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 const envLanguage = "LANGUAGE"
